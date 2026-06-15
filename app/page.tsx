1b4894f7676bb1ab476bcb37d1206241cc768a0a
y4gg <contact@y4.gg>
2026-06-15 22:20:19 +0200

    Refactor links in homepage to use a dynamic items array

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,35 @@
 import Link from "next/link";
 
 export default function Home() {
+  const items = [
+    {
+      name: "Discord Cleaner",
+      url: "https://dcc.y4.gg",
+    },
+    {
+      name: "Gym Ladder",
+      url: "https://gym.y4.gg",
+    },
+    {
+      name: "Github",
+      url: "https://github.com/y4gg",
+    },
+  ];
+
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center">
       <div className="flex flex-col items-start gap-8">
         <h1 className="text-4xl font-extrabold">~ ❯ ls -l</h1>
         <div className="flex w-full flex-col">
-          <Link href="https://dcc.y4.gg" className="animated-link text-xl">
-            Discord Cleaner
-          </Link>
-          <Link
-            href="https://gym-ladder.netlify.app"
-            className="animated-link text-xl"
-          >
-            Gym Ladder
-          </Link>
-          <Link
-            href="https://github.com/y4gg"
-            className="animated-link text-xl"
-          >
-            Github
-          </Link>
+          {items.map((item) => (
+            <Link
+              href={item.url}
+              key={item.url}
+              className="animated-link text-xl"
+            >
+              {item.name}
+            </Link>
+          ))}
         </div>
         <h1 className="text-4xl font-extrabold terminal-text whitespace-pre">
           ~ ❯{" "}
